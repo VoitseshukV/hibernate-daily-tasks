c0e29f9560578f81bc3692f48498d29920e2b64d
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-10-26 22:51:39 +0300

    add DoubleCola implementation by using ariphmetic method

--- src/main/java/core/basesyntax/DoubleCola.java
+++ src/main/java/core/basesyntax/DoubleCola.java
@@ -1,19 +1,12 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
 public class DoubleCola {
   public static String getNthPerson(String[] names, int n) {
-    String current = null;
-    List<String> stack = new LinkedList<>(Arrays.asList(names));
-    for (int i = 0; i < n; i++) {
-      current = stack.get(0);
-      stack.add(current);
-      stack.add(current);
-      stack.remove(0);
+    int iteration = 1;
+    while (n > names.length * iteration) {
+      n -= names.length * iteration;
+      iteration <<= 1;
     }
-    return current;
+    return names[(n - 1) / iteration];
   }
 }
