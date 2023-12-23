1ce945da2c658329be949618aed1e914f117bfe4
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-12-23 17:25:27 +0200

    formatting code

--- src/main/java/core/basesyntax/DoubleCola.java
+++ src/main/java/core/basesyntax/DoubleCola.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class DoubleCola {
-  public static String getNthPerson(String[] names, int n) {
-    int iteration = 1;
-    while (n > names.length * iteration) {
-      n -= names.length * iteration;
-      iteration <<= 1;
+    public static String getNthPerson(String[] names, int n) {
+        int iteration = 1;
+        while (n > names.length * iteration) {
+            n -= names.length * iteration;
+            iteration <<= 1;
+        }
+        return names[(n - 1) / iteration];
     }
-    return names[(n - 1) / iteration];
-  }
 }
